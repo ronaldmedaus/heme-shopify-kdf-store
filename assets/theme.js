92b7899737d1498a9a4f32f48b1dd1e06d0a1ff8
khsolangi <khsolangi3gmail.com>
2022-08-10 15:01:31 +0200

    Paramettrage du logo

--- assets/theme.js
+++ assets/theme.js
@@ -1,1 +1,18 @@
 // Put your application javascript here
+function openMenu() {
+    const navbar = document.querySelector('.navbar');
+    const navbarBtn = document.querySelector('.navbar-toggler');
+
+    if (navbar.classList.contains('open')) {
+        navbar.classList.remove('open');
+        navbarBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    }
+    else {
+        navbar.classList.add('open');
+        navbarBtn.innerHTML = '<i class="fa-solid fa-times"></i>';
+    }
+}
+
+function searchBox() {
+    document.getElementById('searchbox').classList.toggle('show');
+}
